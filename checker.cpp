d11339dd6935a9179440930b59048de512e63df9
coder114514 <98397499+coder114514@users.noreply.github.com>
2023-02-13 12:17:41 +0800

    Update checker.cpp

--- checker.cpp
+++ checker.cpp
@@ -1,26 +1,26 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(int argc,char* argv[]){
-    if(argc!=6){
-        printf("usage: checker <datagen.cpp> <bf.cpp> <solver.cpp> <bf.out> <solver.out>");
+int main(int argc, char *argv[]) {
+    if (argc != 6) {
+        printf("usage: checker <datagen.cpp> <ansgen.cpp> <solver.cpp> <ans.out> <solver.out>");
         return 1;
     }
     char cmd[114];
     sprintf(cmd, "g++ %s -o gen.exe -std=c++11", argv[1]);
     system(cmd);
-    sprintf(cmd, "g++ %s -o bf.exe -std=c++11", argv[2]);
+    sprintf(cmd, "g++ %s -o ansgen.exe -std=c++11", argv[2]);
     system(cmd);
     sprintf(cmd, "g++ %s -o solver.exe -std=c++11", argv[3]);
     system(cmd);
-    int n=1;
-    while(true){
-        printf("#%d:\n",n++);
+    int n = 1;
+    while (true) {
+        printf("#%d:\n", n++);
         system(".\\gen.exe");
-        system(".\\bf.exe");
+        system(".\\ansgen.exe");
         system(".\\solver.exe");
         sprintf(cmd, "fc %s %s", argv[4], argv[5]);
-        if(system(cmd)){
+        if (system(cmd)) {
             break;
         }
     }
