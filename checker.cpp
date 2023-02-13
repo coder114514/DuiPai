b859f2a9c06113d5b90fcedd421fbc85d6e9581c
coder114514 <98397499+coder114514@users.noreply.github.com>
2023-02-13 12:35:11 +0800

    Update checker.cpp

--- checker.cpp
+++ checker.cpp
@@ -8,11 +8,17 @@ int main(int argc, char *argv[]) {
     }
     char cmd[114];
     sprintf(cmd, "g++ %s -o gen.exe -std=c++11", argv[1]);
-    system(cmd);
+    if (system(cmd)) {
+        return 1;
+    }
     sprintf(cmd, "g++ %s -o ansgen.exe -std=c++11", argv[2]);
-    system(cmd);
+    if (system(cmd)) {
+        return 1;
+    }
     sprintf(cmd, "g++ %s -o solver.exe -std=c++11", argv[3]);
-    system(cmd);
+    if (system(cmd)) {
+        return 1;
+    }
     int n = 1;
     while (true) {
         printf("#%d:\n", n++);
